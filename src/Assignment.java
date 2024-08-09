c0a01f4b78616281c842a430fc0f2a54d9de1ac7
David Peterson <peterdav000@warren.k12.in.us>
2024-08-09 13:41:55 -0400

    0000

---
+++ src/Assignment.java
@@ -0,0 +1,15 @@
+public class Assignment {
+
+    public static void main(String[] args){
+}
+
+
+        double v1 = 20.00;
+        double v2 = 80.00;
+        double v = (v1 + v2) * 100.00;
+        double r = v % 40.00;
+        boolean isNoRemainder = (r == 0.00) ? true : false;
+        System.out.println("isNoRemainder = " + noR);
+        if(!inoR) {
+            System.out.println("Got some remainder");
+        }
